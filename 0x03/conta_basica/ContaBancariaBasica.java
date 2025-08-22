f0c45948e10240f56cf519c1dca63260a8221fb0
evyfarias <evelyn.fsilva@sptech.school>
2025-08-22 18:42:03 +0000

    Adicionando conta basica

---
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,62 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+    private String numeracao;
+    private double saldo;
+    private double taxaJurosAnual;
+
+    public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {
+        this.numeracao = numeracao;
+        this.taxaJurosAnual = taxaJurosAnual;
+        this.saldo = 0.0;
+    }
+
+    public String getNumeracao() {
+        return numeracao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getTaxaJurosAnual() {
+        return taxaJurosAnual;
+    }
+
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0) {
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+        }
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+        }
+        if (valor > this.saldo) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }
+        this.saldo -= valor;
+    }
+
+    public double calcularTarifaMensal() {
+        double tarifa = this.saldo * 0.10;
+        if (tarifa > 10.0) {
+            return 10.0;
+        }
+        return tarifa;
+    }
+
+    public double calcularJurosMensal() {
+        if (this.saldo <= 0) {
+            return 0.0;
+        }
+        double taxaMensal = this.taxaJurosAnual / 12.0 / 100.0;
+        return this.saldo * taxaMensal;
+    }
+
+    public void aplicarAtualizacaoMensal() {
+        this.saldo = this.saldo - calcularTarifaMensal() + calcularJurosMensal();
+    }
+}
