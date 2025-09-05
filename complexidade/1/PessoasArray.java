880b8616d1c8856ea4f54e752b915ec7eba2eb53
evyfarias <evelyn.fsilva@sptech.school>
2025-09-05 23:35:40 +0000

    Complexidade 1

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -17,11 +17,9 @@ public class PessoasArray {
         this.nomes = nomes;
     }
 
-    // implementação da busca linear
-    public void buscaLinear(String nome) {
+    public void buscaLinear(String nome){
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         boolean encontrado = false;
-
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i].equals(nome)) {
@@ -29,9 +27,6 @@ public class PessoasArray {
                 encontrado = true;
             }
         }
-
-        if (!encontrado) {
-            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
-        }
+        if(!encontrado) throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
     }
 }
