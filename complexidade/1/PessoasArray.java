da162a2745a73db2f69b841dd061e697b1cbc8e9
evyfarias <evelyn.fsilva@sptech.school>
2025-09-05 23:29:03 +0000

    Complexidade 1

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -1,18 +1,16 @@
-public class PessoasArray {
+public void buscaLinear(String nome) {
+    System.out.println("Procurando pelo nome: \"" + nome + "\"");
+    int posicaoEncontrada = -1;
 
-    public static int buscaLinear(String[] nomes, String nome) {
-        int posicaoEncontrada = -1;
-
-        System.out.println("Procurando pelo nome: \"" + nome + "\"");
-        for (int i = 0; i < nomes.length; i++) {
-            System.out.println("Passando pelo indice:" + i);
-            if (nomes[i].equals(nome)) {
-                posicaoEncontrada = i;
-                System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
-                // não interrompe o loop, continua até o final
-            }
+    for (int i = 0; i < nomes.length; i++) {
+        System.out.println("Passando pelo indice:" + i);
+        if (nomes[i].equals(nome)) {
+            posicaoEncontrada = i;
+            System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
         }
+    }
 
-        return posicaoEncontrada; // -1 caso não encontre
+    if (posicaoEncontrada == -1) {
+        throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
     }
 }
--- complexidade/1/Program.java
+++ complexidade/1/Program.java
@@ -1,18 +1,14 @@
 public class Program {
+
     public static void main(String[] args) {
-        String[] nomes = {
-            "Maria", "João", "Carlos", "Ana", "Paulo", "Fernanda", 
-            "Ricardo", "Juliana", "Isabela", "Roberto", "Lucas", "Mariana",
-            "Patrícia", "Otavio", "Sofia", "Gabriel", "Mateus", "Beatriz",
-            "Larissa", "Pedro", "Camila", "Rafael", "Eduardo"
-        };
-
-        long inicio = System.nanoTime();
-        PessoasArray.buscaLinear(nomes, "Otavio");
-        PessoasArray.buscaLinear(nomes, "Isabela");
-        PessoasArray.buscaLinear(nomes, "Anderson");
-        long fim = System.nanoTime();
-
-        System.out.println("Tempo de execução: " + (fim - inicio));
+        long inicio  = System.nanoTime();
+        try {
+            PessoasArray p = new PessoasArray();
+            p.buscaLinear("Otavio");
+            p.buscaLinear("Isabela");
+            p.buscaLinear("Anderson");
+        }finally {
+            System.out.println("Tempo de execução: "+(System.nanoTime()-inicio));
+        }
     }
 }
