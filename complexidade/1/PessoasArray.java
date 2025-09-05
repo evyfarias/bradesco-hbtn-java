31dd517162d5b7e7432fd0921fea2f450083aa80
evyfarias <evelyn.fsilva@sptech.school>
2025-09-05 23:27:41 +0000

    Complexidade 1

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -1,38 +1,18 @@
 public class PessoasArray {
 
-    private String[] nomes;
+    public static int buscaLinear(String[] nomes, String nome) {
+        int posicaoEncontrada = -1;
 
-    public PessoasArray() {
-        nomes = new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
-                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
-                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
-                "Tais","Umberto","Vanessa","Xavier"};
-    }
-
-    public String[] getNomes() {
-        return nomes;
-    }
-
-    public void setNomes(String[] nomes) {
-        this.nomes = nomes;
-    }
-
-    // Busca Linear - O(n)
-    public void buscaLinear(String nome) {
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
-        boolean encontrado = false;
-
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i].equals(nome)) {
+                posicaoEncontrada = i;
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
-                encontrado = true;
-                break;
+                // não interrompe o loop, continua até o final
             }
         }
 
-        if (!encontrado) {
-            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
-        }
+        return posicaoEncontrada; // -1 caso não encontre
     }
 }
