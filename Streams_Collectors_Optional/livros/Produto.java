5b754ea4dd408645f5ddd03182b5759656b288f4
evyfarias <evelyn.fsilva@sptech.school.com>
2025-09-20 02:37:01 +0000

    stream 0

--- Streams_Collectors_Optional/livros/Produto.java
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Produto {
     private int codigo;
     private String nome;
@@ -29,6 +31,7 @@ public class Produto {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
+        return String.format(Locale.getDefault(), "[%d] %s %s R$ %.2f", codigo, nome, categoria, preco)
+                     .replace('.', ',');
     }
 }
