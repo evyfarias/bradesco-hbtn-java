5f10d3f544441a3f1da0385037a1a5b0c3d508e1
evyfarias <evelyn.fsilva@bradesco.com.br>
2025-10-11 02:58:12 +0000

    add

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,18 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        Produtor produtor = new Produtor(buffer);
+        Consumidor consumidor = new Consumidor(buffer);
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
