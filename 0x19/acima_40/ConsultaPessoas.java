4d432c16605d6896113d4a9ec0ddf2c9ff7fb275
evyfarias <evelyn.fsilva@bradesco.com.br>
2025-10-11 02:17:10 +0000

    add acima 40

--- 0x19/acima_40/ConsultaPessoas.java
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -1,26 +1,34 @@
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.Collectors;
 
 public class ConsultaPessoas {
 
     public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
         return pessoas.stream()
-            .collect(Collectors.groupingBy(Pessoa::getCargo));
+                .collect(Collectors.groupingBy(Pessoa::getCargo));
     }
 
     public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
         return pessoas.stream()
-            .collect(Collectors.groupingBy(
-                Pessoa::getCargo,
-                Collectors.mapping(Pessoa::getIdade, Collectors.toList())
-            ));
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.mapping(Pessoa::getIdade, Collectors.toList())
+                ));
     }
 
     public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
         return pessoas.stream()
-            .collect(Collectors.groupingBy(
-                Pessoa::getCargo,
-                Collectors.filtering(p -> p.getIdade() >= 40, Collectors.toList())
-            ));
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.filtering(p -> p.getIdade() >= 40, Collectors.toList())
+                ));
+    }
+
+    public static Map<String, Long> obterQuantidadePorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.filtering(p -> p.getIdade() >= 40, Collectors.counting())
+                ));
     }
 }
