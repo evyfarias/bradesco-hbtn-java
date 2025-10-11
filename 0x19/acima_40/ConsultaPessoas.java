70f233b2d2375bd2eaca9537dda04e55d23e1ff5
evyfarias <evelyn.fsilva@bradesco.com.br>
2025-10-11 02:15:04 +0000

    add acima 40

--- 0x19/acima_40/ConsultaPessoas.java
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -3,11 +3,13 @@ import java.util.stream.*;
 
 public class ConsultaPessoas {
 
+    // 0. Agrupa pessoas por cargo
     public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(Pessoa::getCargo));
     }
 
+    // 1. Lista idades das pessoas agrupadas por cargo
     public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(
@@ -16,6 +18,7 @@ public class ConsultaPessoas {
             ));
     }
 
+    // 2. Agrupa pessoas por cargo e filtra idade >= 40
     public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(
@@ -24,6 +27,7 @@ public class ConsultaPessoas {
             ));
     }
 
+    // 3. Conta quantidade de pessoas por cargo com idade >= 40 (opcional, se precisar)
     public static Map<String, Long> obterQuantidadePorCargoAcimaDe40anos(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(
