4153f692baf5c70e12ff7c699e775e4c3b75f269
evyfarias <evelyn.fsilva@bradesco.com.br>
2025-10-11 02:02:44 +0000

    add acima 40

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class ConsultaPessoas {
+    public static Map<String, Long> obterQuantidadePorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                Collectors.filtering(
+                    p -> p.getIdade() >= 40,
+                    Collectors.counting()
+                )
+            ));
+    }
+}
